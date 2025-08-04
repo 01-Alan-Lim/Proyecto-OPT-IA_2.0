ec0b1e2deb283933ef6f21fb0781965fe45d2657
unknown <alimachi.404@gmail.com>
2025-08-04 00:54:58 -0400

    Avance 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || '';
+const API_URL = process.env.REACT_APP_API_URL || 'https://app-funtion-gnakajc6h7bkhyas.swedencentral-01.azurewebsites.net/api/';
 
 // Configuración global de axios
 const apiClient = axios.create({
