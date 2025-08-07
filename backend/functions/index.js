8ecb75fbafe0162cee6576d33aaa9cb2a5ba649c
unknown <alimachi.404@gmail.com>
2025-08-07 18:55:09 -0400

    Cambios search

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -4,8 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 const config = {
     endpoint: process.env.AZURE_OPENAI_ENDPOINT,
     apiKey: process.env.AZURE_OPENAI_KEY,
-    deploymentName: process.env.AZURE_OPENAI_DEPLOYMENT,
-    apiVersion: "2024-05-01-preview", // Se actualiza a esta versión para usar data_sources
+    deploymentName: process.env.AZURE_OPENAI_DEPLOYMENT_NAME,
+    apiVersion: "2024-01-01-preview", // Se actualiza a esta versión para usar data_sources
     responseStyles: {
         default: `Instrucciones para el Agente OPT-IA
 
