828e8f4142fd1f6df1ae1fc20a65bea172ec76ea
unknown <alimachi.404@gmail.com>
2025-08-07 14:47:10 -0400

    resuelto 22

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'https://app-funtion-gnakajc6h7bkhyas.swedencentral-01.azurewebsites.net/api/';
->>>>>>> c336cbdadcb8ed26f6ebc060067ee70abf2dd641
 
 const apiClient = axios.create({
     baseURL: API_URL,
