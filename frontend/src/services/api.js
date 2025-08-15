2c9b15862812b1b7276ab279fc8609ed2247a6b3
unknown <alimachi.404@gmail.com>
2025-08-14 20:14:07 -0400

    cambio cambio

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:7071/api/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://app-funtion-gnakajc6h7bkhyas.swedencentral-01.azurewebsites.net/api/';
 
 const apiClient = axios.create({
     baseURL: API_URL,
