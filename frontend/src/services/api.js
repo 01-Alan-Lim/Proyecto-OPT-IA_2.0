9356903052bd6a005dabe9661ac61542e467602e
unknown <alimachi.404@gmail.com>
2025-08-15 00:16:12 -0400

    Cambio 3.0

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://app-funtion-gnakajc6h7bkhyas.swedencentral-01.azurewebsites.net/api/';
+const API_URL = process.env.REACT_APP_API_URL || 'https://app-funtion-gnakajc6h7bkhyas.swedencentral-01.azurewebsites.net/api/history';
 
 const apiClient = axios.create({
     baseURL: API_URL,
